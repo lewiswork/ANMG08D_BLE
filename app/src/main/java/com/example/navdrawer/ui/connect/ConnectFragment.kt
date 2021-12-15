09805c64e8e4dbed7d9ac293c4ed91f443bee9c5
lewiswork <kimyk@adsemicon.com>
2021-12-15 15:43:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/navdrawer/ui/connect/ConnectFragment.kt
@@ -0,0 +1,44 @@
+package com.example.navdrawer.ui.connect
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.navdrawer.databinding.FragmentConnectBinding
+
+class ConnectFragment : Fragment() {
+
+    private lateinit var connectViewModel: ConnectViewModel
+    private var _binding: FragmentConnectBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        connectViewModel =
+            ViewModelProvider(this).get(ConnectViewModel::class.java)
+
+        _binding = FragmentConnectBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        connectViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
